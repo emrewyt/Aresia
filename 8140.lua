bae7db577a81f7e96aad42c14a650358903fe5f3
emrewyt <umutemreyilman66@gmail.com>
2026-09-19 20:08:20 +0300

    Auto-upload manifests for 8140

---
+++ 8140.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(8140) -- Tomb Raider: Underworld
+
+-- MAIN APP DEPOTS
+addappid(8141, 1, "cea2c65903e50bec294289d441b22b0f9c65f6e6aad83700c30c174b312fe36e") -- tomb raider: underworld content
+setManifestid(8141, "495933899943708274", 0)
