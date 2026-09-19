a92c6471ab667079120b97e0f47ed50d306c2153
emrewyt <umutemreyilman66@gmail.com>
2026-09-20 01:26:17 +0300

    Auto-upload manifests for 503580

---
+++ 503580.lua
@@ -0,0 +1,12 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is "f1uxin" on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free!).
+
+-- MAIN APPLICATION
+addappid(503580) -- Duck Season
+
+-- MAIN APP DEPOTS
+addappid(503581, 1, "67d2bc7b42d425dcce983a84d3f2f822f6bc7cde5f7e20cfc026748988bfcdca") -- Duck Season Content
+setManifestid(503581, "1612803388660264694", 0)
