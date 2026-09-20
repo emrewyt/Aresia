1fcd4a66e87dd9216581001196a020576c2c22f0
emrewyt <umutemreyilman66@gmail.com>
2026-09-20 04:38:21 +0300

    Auto-upload manifests for 514830

---
+++ 514830.lua
@@ -0,0 +1,18 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪 / username - f1uxin. On discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my (F1uxins) official server: https://discord.gg/NmmpgnAgen
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(514830) -- Stifled
+-- MAIN APP DEPOTS
+addappid(514831, 1, "97dbc978bd44868a307bd3263e3ba47c524608169bc544b2f9f7458b2e0560e6") -- Stifled Content
+setManifestid(514831, "773658516838689626", 0)
+addappid(514832, 1, "e0778801cdbea1c487aaadec6b120953697a051da9e00f644a80256d57085848") -- Stifled Windows
+setManifestid(514832, "3347420303025271245", 7044941245)
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
