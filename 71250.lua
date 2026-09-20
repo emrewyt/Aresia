5199875e2676bce5ea0e6b19749c74b0abe104ac
emrewyt <umutemreyilman66@gmail.com>
2026-09-20 06:20:31 +0300

    Auto-upload manifests for 71250

---
+++ 71250.lua
@@ -0,0 +1,18 @@
+-- Made by f1uxin, please read the (README) file and enjoy!
+-- P.S. If there is no README file, this file has most likely been stolen the creator of these files is @𓆩𝙁𝟙𝙪𝙭𝙞𝙣𓆪username - f1uxin, on discord, nobody else.
+-- If you want to redistribute/share these files, read the README TXT for more info.
+-- Feel free to join my(F1uxins) official server: https://discord.gg/planetofpiracy / https://discord.gg/m8PxjTwrnv
+-- (We show everything about piracy, including manifest & lua stuff also known as the "Steam method", all for free, Nobody should need to pay for info that is spread and shown for free.)
+
+-- MAIN APPLICATION
+addappid(71250) -- Sonic Adventure DX
+
+-- MAIN APP DEPOTS
+addappid(71251, 1, "772e98e07c55baadc311feeb6fbf98898744f6e56ec8220e4d35f56752bc6716") -- Sonic Adventure DX content
+setManifestid(71251, "3248206210397098900", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
